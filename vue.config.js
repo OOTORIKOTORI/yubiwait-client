26b9995e17a466ff85a66ab7c7ad73eae65792da
OOTORIKOTORI <s0a1t0o8shi@gmail.com>
2025-07-20 15:01:28 +0900

    config changed

--- vue.config.js
+++ vue.config.js
@@ -1,23 +1,7 @@
-// vue.config.js
 const { defineConfig } = require('@vue/cli-service')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const path = require('path')
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  configureWebpack: {
-    plugins: [
-      new CopyWebpackPlugin({
-        patterns: [
-          {
-            from: path.resolve(__dirname, 'public/_redirects'),
-            to: path.resolve(__dirname, 'dist/_redirects'),
-            toType: 'file'
-          }
-        ]
-      })
-    ]
-  },
   devServer: {
     proxy: {
       '/api': {
