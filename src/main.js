96be5f40e72e91783595e209b68307e60d17524f
OOTORIKOTORI <s0a1t0o8shi@gmail.com>
2025-07-20 15:52:39 +0900

    サーバー連携

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+
+axios.defaults.baseURL = process.env.VUE_APP_API_URL
 
 const app = createApp(App)
 app.use(router)
